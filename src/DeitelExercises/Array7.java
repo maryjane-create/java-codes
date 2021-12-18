6ea4879f2da871fecf45991fd8429e2aca74b473
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2021-12-18 08:16:57 +0100

    recent works on arrays

--- src/DeitelExercises/Array7.java
+++ src/DeitelExercises/Array7.java
@@ -1,4 +1,73 @@
 package DeitelExercises;
 
 public class Array7 {
+
+    public  static  void displayArrayElements(int [] []array){
+        int[][] numbers= {{5, 1, 3, 8}, {3, 5, 9}, {4, 8,4}};
+        for (int row =0; row< numbers.length; row++){
+            System.out.println();
+            for (int column =0; column<numbers[row].length; column++){
+                System.out.printf("%d ", numbers[row][column]);
+            }
+
+        }
+        System.out.println();
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        int[][] numbers= {{5, 1, 3, 8}, {3, 5, 9}, {4, 8,4}};
+
+
+        displayArrayElements(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int [][] numbers = {{33,46, 67, 85, 39}, {67,33,21,89,42}, {56,31,9}};
+//displayArrayElements(numbers);
+//    }
+//
+//    public  static void displayArrayElements(int [][]array){
+//        int [][] numbers = {{33,46, 67, 85, 39}, {67,33,21,89,42}, {56,31,9}};
+//        for (int row=0; row< numbers.length; row++){
+//            for (int column =0; column<array[row].length; column++){
+//                System.out.printf("%d ", array[row][column]);
+//            }
+//            System.out.println();
+//        }
+
+    }
+
 }
---
+++ src/DeitelExercises/Array8.java
@@ -0,0 +1,64 @@
+package DeitelExercises;
+
+public class Array8 {
+    public static void main(String[] args) {
+
+
+        int[][] numbers = new int[5][4];
+
+        editARowInTheArray(numbers);
+
+
+
+
+
+
+//for (int row=0;row< numbers.length; row++){
+//    for (int column=0; column<numbers[row].length;column++){
+//        int[2][int column]=3;
+//    }
+//}
+
+    }
+
+    public static void editARowInTheArray(int [][] array){
+
+
+        for (int column =0; column<array[3].length;column++){
+            array[3][column]=9;
+            System.out.printf("%d ", array[3][column]);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        for (int column =0; column<array[2].length;column++){
+//            array[2][column]=11;
+//            System.out.printf("%d ", array[0][column]);
+//
+//
+//        }
+
+
+
+//        for (int row =0; row< array[3].length;row++){
+//            array[row][2]=10;
+//            for (int column=0; column<array[row].length; column++){System.out.printf("%d ", array[4][3]);}
+//            System.out.println();
+//        }
+
+
+    }
+
+}
