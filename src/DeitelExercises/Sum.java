d90a3fc20560642d96c2d92340633c14fc7a1991
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2021-12-21 11:46:36 +0100

    xo and yo

---
+++ src/DeitelExercises/Sum.java
@@ -0,0 +1,17 @@
+package DeitelExercises;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+        int xo=6;
+        int yo =8;
+        int sum = xo + yo;
+
+
+        System.out.printf("the sum of xo and yo is %d", sum);
+
+
+    }
+
+}
