168ec8f404c5be2fc02c18243a45e8353ea2807a
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2021-12-23 09:37:55 +0100

    passing methods to array

---
+++ src/DeitelExercises/DoWhile.java
@@ -0,0 +1,27 @@
+package DeitelExercises;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i =0;
+
+//        while (i<10)
+//        {
+//            System.out.println(i + " is a number");
+//            i++;
+//        }
+
+
+
+
+
+
+
+        do {
+            System.out.println(i + " is a number");
+            i++;
+        }
+        while (i<10);
+    }
+
+
+}
---
+++ src/MyProject/Swap.java
@@ -0,0 +1,37 @@
+package MyProject;
+
+public class Swap {
+    public static void main(String[] args) {
+
+
+        int [] a = {1,2};
+        swap(a[0],a[1] );
+        for (int count =0;count< a.length; count++){
+        System.out.println(a[count]);}
+
+
+
+        System.out.println();
+swap2(a);
+for (int count =0; count< a.length; count++){
+        System.out.println(a[count]);}
+
+    }
+    public  static  void  swap(int num1, int num2){
+        int okay =num2;
+        num1=okay;
+        num2 =num1;
+
+    }
+
+    public static  void swap2(int [] a){
+        int okay =a [0];
+        a[0]=a[1];
+        a[1]=okay;
+
+
+
+
+
+    }
+}
