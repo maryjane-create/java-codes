602b7ddc2660f4a7eca30c8281dc24b8b5532cf0
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-01 02:09:15 +0100

    lop-sided right angled triangle

--- src/DeitelExercises/Triangle3.java
+++ src/DeitelExercises/Triangle3.java
@@ -5,21 +5,16 @@ public class Triangle3 {
 int count=1;
 int num=8;
 
-       for ( count =1; count<=8; count++){
-           System.out.printf("%s", "*");
-       }
 
-        for (int counter=1; counter<=num-1; counter++){
-//            System.out.printf("%s", "*");
 
+//        for (int counter=1; counter<=num-1; counter++){
 for (num=1;num<=7; num++){
-            for (count=1; count<=counter-num;count++) {
+            for (count=1; count<=7-num;count++) {
                 System.out.printf("%s ", "*");
-
             }
     System.out.println();
             }
         }
 
     }
-}
+//}
