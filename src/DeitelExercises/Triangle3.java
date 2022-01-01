539793d49e405cce186eb95dbf6355a44864a8f8
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-01 08:55:10 +0100

    crate rectangle

--- src/DeitelExercises/Triangle3.java
+++ src/DeitelExercises/Triangle3.java
@@ -7,11 +7,23 @@ int num=8;
 
 
 
-//        for (int counter=1; counter<=num-1; counter++){
+        for (int counter=1; counter<=num-1; counter++){
 for (num=1;num<=7; num++){
-            for (count=1; count<=7-num;count++) {
-                System.out.printf("%s ", "*");
-            }
+    System.out.printf("%s", " ");
+//            for (num=1; num<=count-num;num++) {
+//                System.out.printf("%s", "*");}
+//                System.out.println();
+//                for (count=1; count<=8; count++){
+//                    System.out.printf("%s","*");
+//                }
+//                System.out.printf("%s","*");
+//                for (int counter =1; counter<=7-num; counter++){
+//                    System.out.printf("%s ", "*");
+    for (count=1; count<=8; count ++){
+        System.out.printf("%s", "*");
+    }
+              }
+
     System.out.println();
             }
         }
