7bdd76c2f7dfd0e70d5ab76673c5ab9a70e9a871
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-02 20:18:12 +0100

    static method

---
+++ src/DeitelExercises/MathClass.java
@@ -0,0 +1,13 @@
+package DeitelExercises;
+import  static java.lang.Math.*;
+public class MathClass {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%f", sqrt(1000.00));
+        System.out.println();
+        System.out.printf("%f", cbrt(1000.09));
+
+    }
+
+}
