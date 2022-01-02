4cad638cc292e21219a1d556ab2b87e740d083d5
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-02 15:16:16 +0100

    compound interest

--- src/DeitelExercises/Triangle3.java
+++ src/DeitelExercises/Triangle3.java
@@ -2,31 +2,65 @@ package DeitelExercises;
 
 public class Triangle3 {
     public static void main(String[] args) {
-int count=1;
-int num=8;
+int num=1;
 
+int number=0;
 
+//        for (int count =8; count>=1; count--){
+//            System.out.println();
+//            for (int counter =1; counter<=8-count; counter++){
+//                System.out.printf("%s", "*");
+//            }
+            System.out.println();
 
-        for (int counter=1; counter<=num-1; counter++){
-for (num=1;num<=7; num++){
-    System.out.printf("%s", " ");
-//            for (num=1; num<=count-num;num++) {
-//                System.out.printf("%s", "*");}
-//                System.out.println();
-//                for (count=1; count<=8; count++){
-//                    System.out.printf("%s","*");
-//                }
-//                System.out.printf("%s","*");
-//                for (int counter =1; counter<=7-num; counter++){
-//                    System.out.printf("%s ", "*");
-    for (count=1; count<=8; count ++){
-        System.out.printf("%s", "*");
-    }
-              }
+            for ( num=1; num<=8; num++){
+                System.out.printf("%s", "*");
 
-    System.out.println();
             }
+            for (number =1; number<=number-num; number++){
+                System.out.printf("%s", "*");
+                for (int i =1; i<=number-i; i++){
+                    System.out.println();
+                }
+
+            }
+
+
+
+//            System.out.printf("%s", "$");
+//           System.out.printf("%s", "  ");  System.out.printf("%s", " #");
         }
 
+//int count=1;
+//int num=8;
+
+
+//        for (int counter=7; counter>=7; counter--){
+//for (int num=1;num<=7-counter; num++){
+//
+////            for (num=1; num<=count-num;num++) {
+////             System.out.printf("%s", "*");}
+////                System.out.println();
+////                for (count=1; count<=8; count++){
+////                    System.out.printf("%s","*");
+////                }
+////                System.out.printf("%s","*");
+////                for (int counter =1; counter<=7-num; counter++){
+////                    System.out.printf("%s ", "*");
+////    for (count=1; count<=8; count ++){
+////        System.out.printf("%s", "*");
+////    }
+//              }
+//
+//    System.out.println();
+//
+//for (int num =1; num<=counter-num; num++){
+//    System.out.printf("%s %s", " ","*");
+//}
+//            }
+//        }
+//
+//    }
+////}
     }
 //}
---
+++ src/MyProject/BigDecimal1.java
@@ -0,0 +1,84 @@
+package MyProject;
+import java.text.NumberFormat;
+import java.math.BigDecimal;
+import java.time.Year;
+
+public class BigDecimal1 {
+
+    public static void main(String[] args) {
+
+
+
+      BigDecimal principal=BigDecimal.valueOf(6000.00);
+      BigDecimal rate=BigDecimal.valueOf(0.034);
+
+        System.out.printf("%s\t%s\t", "Year", "amount");
+////        for (int year=1; year<=10; year++){
+////            System.out.printf("%n%s", year);
+//        }
+        for (int year =1; year<=10; year++){
+            BigDecimal amount =principal.multiply(rate.add(BigDecimal.ONE).pow(year));
+            System.out.printf("%n%d\t%f\t",year,amount);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        BigDecimal principal=BigDecimal.valueOf(1000.0);
+//        BigDecimal rate=BigDecimal.valueOf(0.05);
+//
+//        System.out.printf("%s\t%s\t", "Year", "amount on deposit");
+//        for (int year=1; year<=5; year++){
+//            System.out.println();
+//            BigDecimal amount=principal.multiply(rate.add(BigDecimal.ONE).pow(year));
+//            System.out.printf("%d\t%s\t", year, NumberFormat.getCurrencyInstance().format(amount));
+//        }
+
+    }
+
+}
