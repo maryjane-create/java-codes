6f4f5af514fd813db59a9edd9fecdd1815df2d5e
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-05 15:18:00 +0100

    package access

--- src/DeitelExercises/PackageDataTest.java
+++ src/DeitelExercises/PackageDataTest.java
@@ -1,20 +1,73 @@
 package DeitelExercises;
 
 public class PackageDataTest {
+
     public static void main(String[] args) {
-        PackageData1 packageData = new PackageData1();
-
-        System.out.printf("packageData");
+        PackageData2 packageData = new PackageData2();
+        packageData.string="dad";
+        packageData.num=44;
+        System.out.printf("%s%n", packageData);
     }
 
 }
 
+     class PackageData2 {
 
-class PackageData{
-    int number=2;
-    String string="Kay";
+        String string="hi";
+        int num= 89;
 
-    public  String toString(){
-        return  String.format("%d %s %n", number, string);
+        public  String toString(){
+            return String.format("%d %s", num, string);
+        }
+
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static void main(String[] args) {
+//        PackageData1 packageData = new PackageData1();
+//packageData.number=89;
+//packageData.string="354";
+//        System.out.printf( "%s",packageData);
+//    }
+//
+//}
+//
+//
+//class PackageData{
+//    int number=2;
+//    String string="Kay";
+//
+//    public  String toString(){
+//        return  String.format("%d %s %n", number, string);
+//    }
