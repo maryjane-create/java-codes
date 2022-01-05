02c01905f895523609000eca93c782c9c51d155f
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-05 10:51:35 +0100

    method calling

---
+++ src/DeitelExercises/EmployeeTest.java
@@ -0,0 +1,29 @@
+package DeitelExercises;
+
+public class EmployeeTest {
+
+
+    public static void main(String[] args) {
+
+        System.out.printf("%d", Employee.getCount());
+        System.out.println();
+Employee e1= new Employee("Janet","Swiss");
+        System.out.println();
+Employee e2=new Employee("Louis", "Ejike");
+        System.out.println();
+        System.out.printf("%d", Employee.getCount());
+    }
+
+
+
+
+
+
+
+
+
+
+//    public static void main(String[] args) {
+//        System.out.printf("%d", Employee.getCount());
+//    }
+}
