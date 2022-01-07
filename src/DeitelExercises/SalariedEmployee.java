88264d06a451e121a5f731bc7c5d351f44bbaecc
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-07 05:17:02 +0100

    polymorphism

---
+++ src/DeitelExercises/SalariedEmployee.java
@@ -0,0 +1,126 @@
+package DeitelExercises;
+
+public abstract class SalariedEmployee extends Employee1 {
+
+    private  double wages;
+    private  double hours;
+
+
+
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber) {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (wages<0.0){
+            throw  new IllegalArgumentException("invalid amount");
+        }
+        this.wages= wages;
+
+        if (hours<0.0||hours>168){
+            throw  new IllegalArgumentException("invalid details");
+        }
+        this.hours= hours;
+    }
+
+    public double getWages() {
+        return wages;
+    }
+
+    public void setWages(double wages) {
+        if (wages<0.0){
+            throw  new IllegalArgumentException("invalid amount");
+        }
+        this.wages = wages;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        if (hours<0.0||hours>168){
+            throw  new IllegalArgumentException("invalid details");
+        }
+        this.hours = hours;
+    }
+
+    @Override
+    public  double earning(){
+        return getHours()*getWages();
+    }
+
+    @Override
+    public  String toString(){
+        return  String.format("%s %f %f", super.toString(), getHours(), getWages());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    private  double hours;
+//    private  double wage;
+//
+//
+//    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, double hours) {
+//        super(firstName, lastName, socialSecurityNumber);
+//        if (hours<0.0 && hours>=168.0){
+//            throw new IllegalArgumentException("hours out of bounds");}
+//        this.hours= hours;
+//
+//        if (wage<0.0){
+//            throw  new IllegalArgumentException("wage is not a valid number");
+//        }
+//        this.wage = wage;
+//    }
+//
+//    public double getHours() {
+//        return hours;
+//    }
+//
+//    public void setHours(double hours) {
+//        if (hours<0.0 || hours>=168.0){
+//            throw new IllegalArgumentException("hours out of bounds");}
+//        this.hours = hours;
+//    }
+//
+//    public double getWage() {
+//        return wage;
+//    }
+//
+//    public void setWage(double wage) {
+//        if (wage<0.0){
+//            throw  new IllegalArgumentException("wage is not a valid number");
+//        }
+//        this.wage = wage;
+//    }
+//
+//
+//    @Override
+//    public  double earning(){return  getHours()*getWage();}
+//
+//    @Override
+//    public  String toString(){
+//        return  String.format("%s  %f %f", super.toString(), getHours(), getWage());
+//    }
+
+}
