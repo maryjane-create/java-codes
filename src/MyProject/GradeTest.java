4851ec0e0f3f55cc0e9da5a85fc296d47bad8f58
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-13 09:33:15 +0100

    added files

--- src/MyProject/GradeTest.java
+++ src/MyProject/GradeTest.java
@@ -6,20 +6,92 @@ public class GradeTest {
 
     public static void main(String[] args) {
 
-        char[][] grade = {{'D', 'C', 'C', 'A', 'D', 'C', 'A', 'B', 'C', 'A'},{'A', 'C', 'C', 'A', 'C', 'C', 'A', 'B', 'D', 'A'}, {'D', 'A', 'C', 'B', 'D', 'D', 'A', 'C', 'C', 'A'}, {'D', 'A', 'C', 'A', 'D', 'C', 'A', 'D', 'C', 'B'}};
+        char[][] grade = {{'A', 'C', 'C', 'A', 'D', 'C', 'A', 'B', 'C', 'A'},{'A', 'C', 'C', 'A', 'C', 'C', 'A', 'B', 'D', 'A'}, {'D', 'A', 'C', 'B', 'D', 'D', 'A', 'C', 'C', 'A'}, {'D', 'A', 'C', 'A', 'D', 'C', 'A', 'D', 'C', 'B'}};
 
 
         char[] key = {'A', 'C', 'C', 'A', 'C', 'C', 'A', 'B', 'D', 'A'};
 
 
-//        for (int row = 0; row < grade.length; row++) {
-//            for (int column = 0; column < grade[row].length; column++) {
-                if (grade[1][0]==key[0]) {
-                    System.out.println(true);
-                } else System.out.println(false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int count =0;
+
+
+        for (int row = 0; row < grade.length; row++) {
+            for (int column = 0; column < grade[row].length; column++) {
+                if (grade[row][column]==key[column]) {
+                    count++;
+                    System.out.println(count);
+                    break;
+                } else System.out.println(false); break;
             }
-//        }
-//    }
+            break;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
