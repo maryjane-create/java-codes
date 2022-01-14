dba1dcdca24324267a2cae10e28cdeda1c8915a8
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-14 15:47:37 +0100

    added files

--- src/MyProject/GPS2.java
+++ src/MyProject/GPS2.java
@@ -1,32 +1,109 @@
 package MyProject;
 
 public class GPS2 {
-    double xCordinate;
-    double yCordinate;
-    public GPS2( double xCordinate, double yCordinate) {
-        this.xCordinate=xCordinate;
-        this.yCordinate=yCordinate;
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+    public GPS2( double x1, double x2, double y1,  double y2) {
+        this.x1=x1;
+        this. x2= x2;
+        this.y1= y1;
+        this.y2=y2;
     }
 
-    public double getxCordinate() {
-        return xCordinate;
+    public double getX1() {
+        return x1;
     }
 
-    public void setxCordinate(double xCordinate) {
-        this.xCordinate = xCordinate;
+    public void setX1(double x1) {
+        this.x1 = x1;
     }
 
-    public double getyCordinate() {
-        return yCordinate;
+    public double getX2() {
+        return x2;
     }
 
-    public void setyCordinate(double yCordinate) {
-        this.yCordinate = yCordinate;
+    public void setX2(double x2) {
+        this.x2 = x2;
     }
 
-    public double getDistance(){
-        return (yCordinate-xCordinate);
+    public double getY1() {
+        return y1;
     }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+
+
+
+
+
+    public  double  findDistanceBetweenPoints(){
+        double distanceBetweenPoints=Math.sqrt(Math.pow(y2-y1, 2)+Math.pow(x2-x1, 2));
+        return distanceBetweenPoints;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public double getxCordinate() {
+//        return xCordinate;
+//    }
+//
+//    public void setxCordinate(double xCordinate) {
+//        this.xCordinate = xCordinate;
+//    }
+//
+//    public double getyCordinate() {
+//        return yCordinate;
+//    }
+//
+//    public void setyCordinate(double yCordinate) {
+//        this.yCordinate = yCordinate;
+//    }
+//
+//    public double getDistance(){
+//        return (yCordinate-xCordinate);
+//    }
 }
 
 
--- test/MyProject/GPS2Test.java
+++ test/MyProject/GPS2Test.java
@@ -1,7 +1,21 @@
 package MyProject;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GPS2Test {
+
+    @BeforeEach
+    public  void findGps(){
+        System.out.println("gps is accurate");
+    }
+
+    @Test
+    public  void weAreCheckingGpsOf(){
+        GPS2 gps2= new GPS2(3, 2, 4, 5);
+        assertEquals(2,1);
+    }
 
 }
