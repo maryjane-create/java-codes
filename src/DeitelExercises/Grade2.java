0d85ffd750e17a497cde5476f7a0e198bcd77247
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-14 17:40:04 +0100

    added files

---
+++ src/DeitelExercises/Grade2.java
@@ -0,0 +1,23 @@
+package DeitelExercises;
+
+public class Grade2 {
+    public static void main(String[] args) {
+int score=0;
+
+        char[] answer = {'a', 'b', 'c', 'd'};
+        char[][] studentScore = {{'a', 'a', 'c', 'd'}, {'d', 'a', 'b', 'c'}, {'a', 'd', 'a', 'c'}};
+
+for (int i=0; i< answer.length;i++){
+    for (int j=0; j< studentScore.length;j++) {
+        if (answer[i] == studentScore[j][i]){
+            score+=1;
+        }
+    }
+}
+
+        System.out.printf("%d", score);
+
+
+
+    }
+}
