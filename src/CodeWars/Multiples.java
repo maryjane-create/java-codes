fb63087855c66b7be774946a42301164b9d2b99a
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-23 08:16:30 +0100

    pushing

--- src/CodeWars/Multiples.java
+++ src/CodeWars/Multiples.java
@@ -2,14 +2,62 @@ package CodeWars;
 
 public class Multiples {
 
-    public int solution(int number) {
-        //TODO: Code stuff here
-        for (int num=1; num<=10; num++){
-            if (num%3==0 || num%5==0){
-                System.out.println(num);
-                return  num;
+
+    public static void main(String[] args) {
+
+        // TODO: Code stuff here
+        int factor=0;
+        for (int num=0; num<=10; num++){
+            if (num%3==0){
+                factor=num;
             }
+            if (num%5==0){
+                factor=num;
+            }
+            for (factor =0; factor<=10; factor++){
+                int sumOfFactors=factor+factor;
+            }
+            System.out.println(factor);
         }
-        return 0;
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+//        for (int num=1; num<=10; num++){
+//        if (num%3==0 || num%5==0){
+//            boolean isTrue=true;
+//            for ( int number=0; number<=10; number++){
+//                if (isTrue){
+//                    System.out.println(number);
+//                }
+//                System.out.println("error");
+//            }
+
+
+
+
+
+
+
+
+
+
+        //System.out.printf("%d", solution(10));
+
+
+//
+//    public static int solution(int number) {
+//
+//
+//
+//    }
--- test/CodeWars/MultiplesTest.java
+++ test/CodeWars/MultiplesTest.java
@@ -1,7 +1,22 @@
 package CodeWars;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultiplesTest {
+
+    @BeforeEach
+    public  void testThatTheNumberIsAMultiple(){
+        System.out.println("these are the multiples of the numbers");
+    }
+
+//
+//    @Test
+//    public  void testThatItIsTrue(){
+//        Multiples multiples= new Multiples();
+//        assertEquals(5, 4, multiples.solution(10));
+//    }
 
 }
