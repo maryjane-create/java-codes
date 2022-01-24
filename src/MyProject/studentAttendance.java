583e2625f80c4dcc0b2db20d06d3f69e397c334b
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-24 16:52:42 +0100

    added files

---
+++ src/MyProject/studentAttendance.java
@@ -0,0 +1,17 @@
+package MyProject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class studentAttendance {
+
+    public static void main(String[] args) {
+
+
+        ArrayList<String> student = new ArrayList<String>();
+student.add("Sope");
+student.add("Temidire");
+        System.out.println(student);
+
+    }
+}
