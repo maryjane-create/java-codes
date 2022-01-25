df34f9b27468927105dad582bd88d92ca6af5376
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-25 05:29:31 +0100

    added files

--- src/MyProject/Multiples2.java
+++ src/MyProject/Multiples2.java
@@ -5,6 +5,7 @@ public class Multiples2 {
 
     public static void main(String[] args) {
         findTheFactorialOf(3);
+        findTheFakeFibonacciOf(5);
     }
 
 
@@ -15,9 +16,18 @@ public class Multiples2 {
         for (int i=1; i<=number; i++){
              factorial = i*factorial;
         }
-        System.out.println(factorial);
+        //System.out.println(factorial);
         return factorial;
 
+    }
+
+
+
+
+    public  static  int findTheFakeFibonacciOf(int num){
+        int result = (int) (findTheFactorialOf(num)*Math.PI);
+        System.out.println(result);
+        return  result;
     }
 
 
