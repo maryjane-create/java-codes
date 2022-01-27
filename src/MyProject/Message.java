543f173fa4baab226e0719420058f73104280097
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-27 13:09:31 +0100

    added files

---
+++ src/MyProject/Message.java
@@ -0,0 +1,18 @@
+package MyProject;
+
+import java.util.ArrayList;
+
+public class Message {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> message = new ArrayList<String>();
+
+        message.add("they want to eat");
+        message.add("they are hungry");
+
+        System.out.println(message);
+        System.out.println(message.size());
+
+    }
+}
--- src/MyProject/String3.java
+++ src/MyProject/String3.java
@@ -6,9 +6,15 @@ public class String3 {
     public static void main(String[] args) {
 
         char[] letterOfName={'s', 'a', 'm', 'u', 'e', 'l'};
+        char[] letterOfAnimal={'s', 'a', 'l', 'm', 'o', 'n'};
+        char[] letterOfName2={'s', 'a', 'm', 'u', 'e', 'l'};
+
 
         String name= new String(letterOfName);
         System.out.println(letterOfName);
+
+
+        System.out.println(letterOfName==letterOfName2);
 
 
 
