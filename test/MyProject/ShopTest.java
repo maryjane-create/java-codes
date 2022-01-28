1c58ca9e28184d6952ab691eb2fb4e10d991cf1d
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-29 00:53:38 +0100

    added files

--- test/MyProject/ShopTest.java
+++ test/MyProject/ShopTest.java
@@ -8,15 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShopTest {
 
     @BeforeEach
-    public  void  assertThatShopExists(){
+    public void assertThatShopExists() {
         System.out.println("true");
     }
 
     @Test
-    public  void  testThatShopExists(){
-        Shop shop= new Shop("banana");
+    public void testThatShopExists() {
+        Shop shop = new Shop("banana");
         assertEquals(30, 70, shop.getAmount());
         System.out.println(shop.getAmount());
-        }
     }
 
+
+    @Test
+    public void testThatYouEnteredGoodsInTheCounter() {
+        Shop shop1= new Shop("");
+        assertEquals("orange", "orange", shop1.getItemName());
+        System.out.println(shop1.getItemName());
+
+    }
+}
