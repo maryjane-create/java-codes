7a4979a4e269b4c51d04f2e857f4b1dccdfcca4e
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-29 14:44:57 +0100

    added files

--- test/Assignment/HomeAddressTest.java
+++ test/Assignment/HomeAddressTest.java
@@ -1,7 +1,15 @@
 package Assignment;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeAddressTest {
+    @Test
+    public  void weWantToTestThatCountryIsNigeria(){
+        HomeAddress homeAddress=new HomeAddress("", "", "", 99, "");
+        assertEquals("Nigeria", "Nigeria", homeAddress.getCountryName());
+
+    }
 
 }
