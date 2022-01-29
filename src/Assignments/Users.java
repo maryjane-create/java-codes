761a0c553d2d544b728eaf9b699f19c5667790d0
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-01-29 07:32:08 +0100

    added files

---
+++ src/Assignments/Users.java
@@ -0,0 +1,26 @@
+package Assignments;
+
+public interface Users {
+
+
+
+    String name="";
+    String phoneNumber="";
+    int age=0;
+    String emailAddress="";
+    String homeAddress="";
+    String password="";
+
+
+
+
+    void  accessGoods();
+
+
+
+
+
+
+
+
+}
