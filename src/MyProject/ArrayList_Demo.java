c6176ec7882458881e92ad9738bf8221399871f5
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-02-03 17:23:19 +0100

    added files

--- src/MyProject/ArrayList_Demo.java
+++ src/MyProject/ArrayList_Demo.java
@@ -4,7 +4,11 @@ public class ArrayList_Demo {
     int num;
     String [] array=new String[num];
 
-
+    public static void main(String[] args) {
+        String [] letters={"dk", "hh", "jj"};
+        ArrayList_Demo arrayList_demo=new ArrayList_Demo(6, letters);
+        arrayList_demo.addElement("for", 0);
+    }
 
     public ArrayList_Demo(int num, String [] array) {
         this.num = num;
@@ -27,13 +31,14 @@ public class ArrayList_Demo {
         this.array = array;
     }
 
-    public String[] addElement(String element, int index){
-        if (index< array.length){
-            array[index]=element;
+    public void addElement(String element, int index){
+//        while (index< array.length){
+         array[index]=element;
+            for (int i=0; i<array.length; i++){
+                System.out.println(array[i]);
+
+            }
         }
-
-
-        return array;
     }
 
 
@@ -45,4 +50,4 @@ public class ArrayList_Demo {
 
 
 
-}
+//}
