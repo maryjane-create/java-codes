2fb6725d853c356ee60679faabbfd16fcdd811a6
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-03-02 05:49:31 +0100

    added files

--- src/DeitelExercises/nums_20.java
+++ src/DeitelExercises/nums_20.java
@@ -1,4 +1,35 @@
 package DeitelExercises;
 
-public class nums_20 {
-}
+import java.util.Arrays;
+//
+//public class nums_20 {
+//
+//   int[]numArray=new int[20];
+//
+////
+//   public int collectNumber(){
+//      numArray[0]=6;
+//      numArray[1]=5;
+////
+////      for (int i=0; i<=3; i++){
+////
+////      }
+////
+////        return  numArray[i];
+////    }
+////    public  int returnDuplicate() {
+////       int numberDuplicated =0;
+////        Arrays.sort(numArray);
+////        for (int i=0; i <= 20; i++) {
+////            if (numArray[i]==(numArray[i+1])) {
+////                numberDuplicated = numArray[i];
+////            }
+////            numberDuplicated =0;
+////        }
+////        return numberDuplicated;
+////    }
+//
+//
+//
+//
+//}
---
+++ test/DeitelExercises/nums_20Test.java
@@ -0,0 +1,24 @@
+//package DeitelExercises;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class nums_20Test {
+//    nums_20 nums20=new nums_20();
+//
+//
+//
+//    @Test
+//    public void  seeThatNumberIsCollected(){
+//
+//        assertEquals(6,nums20.collectNumber());
+//    }
+//
+//    @Test
+//    public  void seeThatThereAreNoDuplicates(){
+//        assertEquals(0, nums20.returnDuplicate());
+//    }
+//
+//
+//}
