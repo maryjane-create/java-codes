37755d89ebc87c086f6b11a984c72b3a2d564303
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-03-26 05:41:06 +0100

    added files

--- test/MyProject/Circle4Test.java
+++ test/MyProject/Circle4Test.java
@@ -1,7 +1,19 @@
 package MyProject;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//import org.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Circle4Test {
+    @Test
+    public  void testForAreaOfCircle(){
+        Circle4 circle4=new Circle4(true);
+        Assertions.assertEquals(true, circle4.isFilled);
+        circle4.setRadius(4);
+        assertEquals( 50.26548245743669, circle4.getArea());
+
+
+    }
 
 }
