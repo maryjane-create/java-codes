1bb4c97e4a4298d5ac544cde0ac36f1f58705672
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-03-29 20:45:44 +0100

    mystery count

--- src/Assignment/MysterySet.java
+++ src/Assignment/MysterySet.java
@@ -1,10 +1,30 @@
 package Assignment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MysterySet {
 
     public String sentence;
+    private Integer countWord;
+    private int numberOfDuplicateWords;
 
     public int countDuplicate() {
-        return  2;
+      String lowerCaseFormat=  sentence.toLowerCase();
+      String[] splitedWords=lowerCaseFormat.split(" ");
+//        ArrayList<String>wordInBasket=new ArrayList<>();
+        HashMap <String, Integer> wordInBasket=new HashMap<>();
+
+        for (String word:splitedWords) {
+
+            wordInBasket.put(word, countWord);
+
+            int wordInBasketCount=wordInBasket.size();
+             numberOfDuplicateWords= splitedWords.length-wordInBasketCount;
+        }
+        return numberOfDuplicateWords;
+
     }
+
+
 }
--- test/Assignment/MysterySetTest.java
+++ test/Assignment/MysterySetTest.java
@@ -13,7 +13,7 @@ class MysterySetTest {
     @Test
             public  void  testThatThereIsDuplicateCount(){
         MysterySet mysterySet = new MysterySet();
-        mysterySet.sentence="The boy is a boy";
+        mysterySet.sentence="The boy is a is boy";
         assertEquals(2, mysterySet.countDuplicate());
 
     }
