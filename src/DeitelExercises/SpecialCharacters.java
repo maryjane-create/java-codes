4e8bf99312cfd7087a7f9fddc30df0df71287351
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-04-21 11:21:04 +0100

    added files

--- src/DeitelExercises/SpecialCharacters.java
+++ src/DeitelExercises/SpecialCharacters.java
@@ -2,6 +2,8 @@ package DeitelExercises;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpecialCharacters {
     public static void main(String[] args) {
@@ -9,6 +11,7 @@ public class SpecialCharacters {
         String word="";
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> arrayOfStrings=new ArrayList<>();
+        ArrayList<String>arrayOfSpecialCharacter= new ArrayList<>();
         for (int i=0; i<3; i++){
             System.out.println("please input strings");
             word=scanner.nextLine();
@@ -16,8 +19,16 @@ public class SpecialCharacters {
         }
         System.out.println(arrayOfStrings);
 
+        for (String element:arrayOfStrings){
+            Pattern pattern= Pattern.compile("[^a-zA-Z0-9]");
+            Matcher matcher=pattern.matcher(element);
+            boolean containsSpecialCharacter= matcher.find();
+            if (containsSpecialCharacter){
+                arrayOfSpecialCharacter.add(element);
+            }
+            System.out.println(arrayOfSpecialCharacter);
 
-
+        }
 
     }
 }
