95e1f341f3b4e96bdb4223217b6ceed6ef6072a2
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-04-22 05:23:52 +0100

    pig latin completed

--- src/DeitelExercises/Piglatin.java
+++ src/DeitelExercises/Piglatin.java
@@ -1,4 +1,22 @@
 package DeitelExercises;
 
+import java.util.Scanner;
+
 public class Piglatin {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter word");
+        String word= scanner.nextLine();
+
+       String pigLatin= word.substring(0, word.length()-1).replace(word.charAt(0), word.charAt(word.length()-1))+word.substring(0, word.length()-2).replace(word.charAt(word.length()-1), word.charAt(0))+"ay";
+        System.out.println(pigLatin);
+
+
+
+
+
+    }
+
 }
