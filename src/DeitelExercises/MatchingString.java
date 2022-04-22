16a5c0ba168b15778303c1ee46ed7e94ae6324d9
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-04-22 18:24:26 +0100

    incomplete matching string

--- src/DeitelExercises/MatchingString.java
+++ src/DeitelExercises/MatchingString.java
@@ -1,4 +1,30 @@
 package DeitelExercises;
 
+import java.util.Scanner;
+
 public class MatchingString {
+
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+            System.out.println("please input first string value");
+            String firstString=scanner.nextLine();
+
+            System.out.println("please input second string value");
+            String secondString= scanner.nextLine();
+
+           if ( firstString.regionMatches(firstString.length(), secondString, 0, firstString.length())){
+               System.out.println("it matches");
+           }
+           else {
+               System.out.println("it doesn't match");
+           }
+
+
+
+
+    }
+
+
 }
