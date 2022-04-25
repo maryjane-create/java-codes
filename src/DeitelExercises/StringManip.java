c5901ba1a79383a080fabcec0f80bdf507d725fe
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-04-25 23:05:27 +0100

    added files

--- src/DeitelExercises/StringManip.java
+++ src/DeitelExercises/StringManip.java
@@ -1,6 +1,22 @@
 package DeitelExercises;
 
-public class StringManip {
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StringManip{
+    private static ArrayList<Character> arrayOfChars= new ArrayList<>();
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter your text");
+        StringBuffer text= new StringBuffer(scanner.nextLine());
+
+        for (int i=0; i< text.length();i++){
+            arrayOfChars.add(text.charAt(i));
+        }
+        System.out.println(arrayOfChars);
 
 
+    }
 }
