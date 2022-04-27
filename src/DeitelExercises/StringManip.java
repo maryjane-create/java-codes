36aea2ba67eb46a3be44269d70cc7adb1c6f0c02
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-04-27 18:13:52 +0100

    added files

--- src/DeitelExercises/StringManip.java
+++ src/DeitelExercises/StringManip.java
@@ -8,12 +8,25 @@ public class StringManip{
 
     public static void main(String[] args) {
 
-     String str="Geek";
+     String str="kaak";
      char[] chars=new  char[str.length()];
 
      for (int i=0; i<str.length();i++){
          chars[i]=str.charAt(i);
-         System.out.println(chars[i]);
+//         System.out.println(chars[i]);
+         for (int j=0; j<chars.length;j++){
+             if (chars[j]==chars[j+1]){
+                 ArrayList <Character>characters=new ArrayList<>();
+                 characters.add(chars[j+1]);
+                 if (j+1== 3){
+                     break;
+                 }
+                 else {
+                     System.out.println(characters);
+
+                 }
+             }
+         }
      }
 
 
