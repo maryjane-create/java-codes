d75f77abec1e17b1fa2f7c2a897f9a8c4ba8ce20
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-05-06 19:23:55 +0100

    generics

--- src/DeitelExercises/FactoryMethods.java
+++ src/DeitelExercises/FactoryMethods.java
@@ -1,6 +1,8 @@
 package DeitelExercises;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class FactoryMethods {
 
@@ -8,6 +10,17 @@ public class FactoryMethods {
 
         List<String> colorList=List.of("blue", "green", "yellow");
         System.out.printf("%s, ", colorList);
+
+        Set<String> colorset=Set.of("blue", "orange", "design");
+        System.out.printf("%s", colorset);
+
+        Map<String, Integer> daysPerMonthMap=Map.ofEntries(
+                Map.entry("January", 31),
+                Map.entry("February", 5),
+                Map.entry("November", 7)
+        );
+
+        System.out.printf("%s", daysPerMonthMap);
 
 
     }
