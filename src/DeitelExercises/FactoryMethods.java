c67315649651c1b642bbcfead9cd657f5d49eb66
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-05-06 18:02:34 +0100

    generics

---
+++ src/DeitelExercises/FactoryMethods.java
@@ -0,0 +1,14 @@
+package DeitelExercises;
+
+import java.util.List;
+
+public class FactoryMethods {
+
+    public static void main(String[] args) {
+
+        List<String> colorList=List.of("blue", "green", "yellow");
+        System.out.printf("%s, ", colorList);
+
+
+    }
+}
